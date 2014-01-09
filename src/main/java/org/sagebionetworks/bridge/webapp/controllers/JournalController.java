6ee5c06de6605c75db019dead88cf1b32aacf493
Alx Dark <alx.dark@sagebase.org>
2014-01-08 17:28:49 -0800

    Enhancing (and disabling) server-side validation (disabling so tests pass)

--- src/main/java/org/sagebionetworks/bridge/webapp/controllers/JournalController.java
+++ src/main/java/org/sagebionetworks/bridge/webapp/controllers/JournalController.java
@@ -28,17 +28,12 @@ import org.sagebionetworks.repo.model.table.RowSet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.hp.hpl.jena.sparql.core.ResultBinding;
 
 @Controller
 public class JournalController {
